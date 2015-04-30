6853cf334aac797c19bf48ce44e021f0b1b6fcf9
Nathan Hoad <nathan@getoffmalawn.com>
2015-04-30 17:50:31 +1000

    Split up tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -29,22 +29,6 @@ def browser_window():
     return BrowserWindow(roland=MagicMock())
 
 
-@pytest.mark.parametrize('bytecount,expected_output', [
-    (1000, '1000b'),
-    (1024, '1kb'),
-    (10240, '10kb'),
-    (102400, '100kb'),
-    (1024*1024, '1mb'),
-    (1024*1024*512, '512mb'),
-    (1024*1024*1024, '1gb'),
-    (1024*1024*1024*512, '512gb'),
-    (1024*1024*1024*1024, '1tb'),
-])
-def test_pretty_size(bytecount, expected_output):
-    from roland.utils import get_pretty_size
-    assert get_pretty_size(bytecount) == expected_output
-
-
 class TestBrowserCommands:
     @pytest.mark.parametrize('url,new_window', [
         ('', False),
