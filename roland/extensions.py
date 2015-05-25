612e84b601757101f9f30da11fab19694a36c5fb
Nathan Hoad <nathan@getoffmalawn.com>
2015-05-25 23:16:53 +1000

    Don't persist the strange URIs that come about as a result of the web process crashing.

--- roland/extensions.py
+++ roland/extensions.py
@@ -180,7 +180,7 @@ class SessionManager(Extension):
         for window in self.roland.get_windows():
             uri = window.webview.get_uri()
 
-            if uri not in (None, 'about:blank'):
+            if uri not in (None, 'about:blank', 'http:/'):
                 # FIXME: add back/forwards history here?
                 session.append({'uri': uri})
 
