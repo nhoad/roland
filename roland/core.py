c6634a5a22a922baf2e233a7b8fb3ea6ae922a29
Nathan Hoad <nathan@getoffmalawn.com>
2015-07-20 23:42:10 +1000

    Wahoo for old comments.

--- roland/core.py
+++ roland/core.py
@@ -853,7 +853,6 @@ class EntryLine(Gtk.VBox):
 
         return result
 
-    # FIXME: this is a terrible method name. It should be prompt.
     def prompt(self, callback, suggestions=None, force_match=False,
                glob=False, prompt='', initial='', cancel=None,
                case_sensitive=True, beginning=True, private=False):
